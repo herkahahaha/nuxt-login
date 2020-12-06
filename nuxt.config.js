a8556bc3693088213781d295606d78825a1a6a88
Herka Trya Fitra <herkahahaha@gmail.com>
2020-12-07 01:39:14 +0700

    hmm

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,6 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: "static",
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: "%s - app-login-nuxt",
     title: "app-login-nuxt",
@@ -16,14 +14,11 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     {
       src: "~/node_modules/vuetify/dist/vuetify.css"
     }
   ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     {
       src: "~/plugins/vuetify"
@@ -44,11 +39,7 @@ export default {
     // https://go.nuxtjs.dev/vuetify
     "@nuxtjs/vuetify"
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: ["@nuxtjs/axios"],
-
-  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     theme: {
